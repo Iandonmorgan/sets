c4a01826604c923f9b19378052e151ce8a55cb33
Landon Morgan <morganlandon@gmail.com>
2020-04-14 14:37:35 -0500

    practice complete book 1 chapter 7

--- cars.py
+++ cars.py
@@ -7,10 +7,36 @@
 
 # print(languages)
 
-showroom = set()
-
-showroom = { 'Ford Mustang Mach-E', 'Tesla Model X', 'Honda Element', 'Mercedes E320'}
+# showroom = set()
+showroom = { 'Ford Mustang Mach-E', 'Tesla Model X', 'Honda Element', 'Mercedes E320' }
 
 print("SHOWROOM LENGTH: ", len(showroom))
+print("SHOWROOM: ", showroom)
 
-showroom.add()
+showroom.add('Ford Mustang Mach-E')
+
+print("SHOWROOM LENGTH: ", len(showroom))
+print("SHOWROOM: ", showroom)
+
+showroom_update = { 'McLaren P1', 'Ford Pinto' }
+
+showroom.update(showroom_update)
+
+print("SHOWROOM LENGTH: ", len(showroom))
+print("SHOWROOM: ", showroom)
+
+showroom.discard('Tesla Model X')
+
+print("SHOWROOM LENGTH: ", len(showroom))
+print("SHOWROOM: ", showroom)
+
+junkyard = { 'Ford Mustang', 'Tesla Model E', 'McLaren P1', 'Honda Civic', 'Mercedes E320' }
+
+print("JUNKYARD: ", junkyard)
+
+print("OVERLAP: ", showroom.intersection(junkyard))
+
+showroom = showroom.union(junkyard)
+print("SHOWROOM: ", showroom)
+showroom.discard('Tesla Model E')
+print("SHOWROOM: ", showroom)
