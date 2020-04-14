4538c1933eae7adad273f92c55ea7663f060f0ea
Landon Morgan <morganlandon@gmail.com>
2020-04-14 14:08:15 -0500

    practice complete book 1 chapter 7

---
+++ cars.py
@@ -0,0 +1,16 @@
+# Using set() to create a set
+# languages = set()
+
+# Using curly braces allows you to initialize the set with values
+# languages = { 'english', 'mandarin chinese', 'spanish', 'english', 'spanish', 'portugese' }
+
+
+# print(languages)
+
+showroom = set()
+
+showroom = { 'Ford Mustang Mach-E', 'Tesla Model X', 'Honda Element', 'Mercedes E320'}
+
+print("SHOWROOM LENGTH: ", len(showroom))
+
+showroom.add()
